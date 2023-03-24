2544f4505658f808997941394d743388e8d418d3
Tejash Patel <tejpat98@gmail.com>
2023-03-24 12:07:45 +0000

    final

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ const userSchema = mongoose.Schema({
 });
 const userModel = mongoose.model("users", userSchema);
 const exerciseModel = mongoose.model("exercises", exerciseSchema);
-const URI = "mongodb://127.0.0.1/boilerplate-project-exercisetracker";
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
